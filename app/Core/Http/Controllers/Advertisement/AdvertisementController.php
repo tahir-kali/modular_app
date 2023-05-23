0d7e37cb290d9ac7faa274924d625588aa55581a
Codeholic <irenic.tahir@gmail.com>
2023-05-23 17:03:14 +0500

    Basic Ads CRUD WEB+API

--- app/Core/Http/Controllers/Advertisement/AdvertisementController.php
+++ app/Core/Http/Controllers/Advertisement/AdvertisementController.php
@@ -3,9 +3,12 @@
 namespace App\Core\Http\Controllers\Advertisement;
 
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Modules\Advertisement\Actions\GetAction;
+use Modules\Advertisement\Actions\GetOneAdvertisementAction;
 use Modules\Advertisement\Actions\StoreAction;
+use Modules\Advertisement\Entities\Advertisement;
 use Modules\Advertisement\Http\Requests\StoreRequest;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -23,6 +26,11 @@ class AdvertisementController extends Controller
     }
     public function show(GetAction $action){
         return $action->execute();
+    }
+    public function edit(Advertisement $advertisement,GetOneAdvertisementAction $action){
+
+        $ad = $action->execute($advertisement);
+        return view('edit_advertisement',compact('ad'));
     }
 
 }
